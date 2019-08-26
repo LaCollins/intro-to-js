9d17c50013ccc89aa22f5aeae457d4cff8a13b82
Laura Collins <laura.e.collins1@gmail.com>
2019-08-26 18:11:24 -0500

    updating objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+
+const bouncer = (age) => {
+    if (age >= 21) {
+        console.log('DRINK!!');
+    } else {
+        console.log('SHAME!');
+    };
+}
+
+bouncer(12);
+bouncer(93);
+
+const me = 'Martin';
+console.log(me.length);
